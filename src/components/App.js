e6af0d58136c01de53b5aadb307001ce9940a70a
Joyce Lin <joycelin79@gmail.com>
2017-07-23 17:04:59 -0700

    format initial structure

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <div id="intro">
+            <div className="width">
+              <h2>Evolution of Web Server Operating Systems</h2>
+              <h3>
+                by <a href="https://meowsergirl.com/">Joyce Lin</a>
+                <span className="spacer"> | </span>
+                <i className="fa fa-medium fa-lg" /> Read on <a href="https://medium.com/@joycelin.codes/evolution-of-web-server-operating-systems-ba1cf1c1e3d8">Medium</a>
+                <span className="spacer"> | </span>
+                <i className="fa fa-github fa-lg" /> Fork me on <a href="https://github.com/loopDelicious/space-launch">Github</a>
+
+              </h3>
+
+            </div>
+          </div>
+          <div className="intro-p">
+            <div className="width">
+              <div className="section intro-text">
+                <p>Intro text.</p>
+                <p>Have your own story to tell? <a href="https://github.com/loopDelicious/space-launch">Fork this project on GitHub</a> and make your own visualizations.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import './css/reset.css';
+import './css/variables.css';
+import './css/index.css';
+
+import App from './components/App.js';
+import registerServiceWorker from './components/registerServiceWorker.js';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
